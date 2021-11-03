d4ecf16c98e5990bf5a63a1b948660ab2101066c
whiteless9 <52316271+whiteless9@users.noreply.github.com>
2021-11-03 11:10:06 +0800

    Update ddns.py

--- Aliyun-ddns/ddns.py
+++ Aliyun-ddns/ddns.py
@@ -14,8 +14,8 @@ from aliyunsdkalidns.request.v20150109 import DescribeSubDomainRecordsRequest, A
 import json, urllib
 
 # AccessKey 和 Secret  建议使用 RAM 子账户的 KEY 和 SECRET 增加安全性
-ID = 'LTAI5tH43iMw8wa5Zj7hqP2R'
-SECRET = 'j1OmSzlGbx4oaeXNCs3VJWj2S2BjVp'
+ID = '***'
+SECRET = '****'
 
 # 地区节点 可选地区取决于你的阿里云帐号等级，普通用户只有四个，分别是杭州、上海、深圳、河北，具体参考官网API
 regionId = 'cn-hangzhou'
@@ -24,7 +24,7 @@ regionId = 'cn-hangzhou'
 client = AcsClient(ID, SECRET, regionId)
 
 # 设置主域名
-DomainName = 'zhinengjiqiren.top'
+DomainName = '*****.top'
 
 # 子域名列表  列表参数可根据实际需求增加或减少值
 SubDomainList = ['a']
